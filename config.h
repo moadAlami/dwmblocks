c06e16fc2561182d6c18e3aac3330ddf42a505fc
Mouad Alami <moadalami40@gmail.com>
2022-12-04 20:53:35 +0100

    first commit

--- config.h
+++ config.h
@@ -1,16 +1,15 @@
-#define CMDLENGTH 45
+#define CMDLENGTH 100
 #define DELIMITER "  "
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sb-mail",    1800, 17),
-	BLOCK("sb-music",   0,    18),
-	BLOCK("sb-disk",    1800, 19),
-	BLOCK("sb-memory",  10,   20),
-	BLOCK("sb-loadavg", 5,    21),
-	BLOCK("sb-mic",     0,    26),
-	BLOCK("sb-record",  0,    27),
-	BLOCK("sb-volume",  0,    22),
-	BLOCK("sb-battery", 5,    23),
-	BLOCK("sb-date",    1,    24)
+	BLOCK("sb-bluetooth",	1, 8),
+	BLOCK("sb-bandwidth",	1, 7),
+	BLOCK("sb-torrent",	1, 6),
+	BLOCK("sb-memory",	5, 5),
+	BLOCK("sb-volume",	0, 4),
+	BLOCK("sb-cpu",	5, 3),
+	BLOCK("sb-date",	1, 2),
+	BLOCK("sb-numlock", 0, 1),
 };
+
