66370a4ffac91d4bad75c0ae873c01f0851c4fb7
Mouad Alami <moadalami40@gmail.com>
2023-02-20 21:21:20 +0100

    added email module

--- config.h
+++ config.h
@@ -7,12 +7,13 @@ const Block blocks[] = {
 	BLOCK("sb-bluetooth",	1, 8),
 	BLOCK("sb-bandwidth",	1, 7),
 	BLOCK("sb-torrent",	1, 6),
+	BLOCK("sb-email", 120, 12),
 	BLOCK("sb-memory",	5, 5),
 	BLOCK("sb-volume",	0, 4),
 	BLOCK("sb-cpu",	5, 3),
 	BLOCK("sb-battery", 1, 10),
 	BLOCK("sb-date",	60, 2),
-	BLOCK("sb-kblayout", 0, 11)
+	BLOCK("sb-kblayout", 0, 11),
 	/* BLOCK("sb-numlock", 0, 1), */
 };
 
