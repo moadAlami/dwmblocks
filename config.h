45e38a034285c6e115f93570253d0f6ddb3f519d
moadAlami <moadalami40@gmail.com>
2023-11-15 20:59:30 +0100

    removed torrent and numlock from laptop

--- config.h
+++ config.h
@@ -6,7 +6,6 @@ const Block blocks[] = {
 	BLOCK("sb-mpd", 1, 9),
 	BLOCK("sb-bluetooth",	1, 8),
 	BLOCK("sb-bandwidth",	1, 7),
-	BLOCK("sb-torrent",	1, 6),
 	/* BLOCK("sb-email", 120, 12), */
 	BLOCK("sb-memory",	5, 5),
 	BLOCK("sb-volume",	0, 4),
@@ -14,6 +13,5 @@ const Block blocks[] = {
 	BLOCK("sb-battery", 1, 10),
 	BLOCK("sb-date",	60, 2),
 	BLOCK("sb-kblayout", 0, 11),
-	/* BLOCK("sb-numlock", 0, 1), */
 };
 
